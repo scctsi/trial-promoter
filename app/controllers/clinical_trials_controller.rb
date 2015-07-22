03145164fda1f48db1c23cae392e374531ebf47b
Praveen Angyan <angyan@usc.edu>
2015-07-22 16:38:46 -0700

    Fixed small bug for selection for randomization

--- app/controllers/clinical_trials_controller.rb
+++ app/controllers/clinical_trials_controller.rb
@@ -1,5 +1,8 @@
 class ClinicalTrialsController < ApplicationController
   def index
-    @clinical_trials = ClinicalTrial.all
+    @all_trials = ClinicalTrial.all.order(:nct_id)
+    @selected_trials = ClinicalTrial.where(:randomization_status => 'Selected')
+    @control_trials = ClinicalTrial.where(:randomization_status => 'Control')
+    @unused_trials = ClinicalTrial.where(:randomization_status => 'Unused')
   end
 end
--- app/models/clinical_trial.rb
+++ app/models/clinical_trial.rb
@@ -5,6 +5,11 @@ class ClinicalTrial < ActiveRecord::Base
   validates :title, :presence => true
   validates :url, :presence => true
 
+  def ==(other_trial)
+    # TODO: Unit test
+    self.nct_id == other_trial.nct_id
+  end
+
   def self.select_for_randomization
     # TODO: Unit test
     random = Random.new
@@ -12,27 +17,27 @@ class ClinicalTrial < ActiveRecord::Base
     clinical_trials = ClinicalTrial.all
 
     # Sample the 46 trials we need for randomization
-    trials_for_randomization = ClinicalTrial.sample(46, random)
+    trials_for_randomization = ClinicalTrial.all.to_a.sample(46, random: random)
     trials_for_randomization.each do |clinical_trial|
       clinical_trial.randomization_status = 'Selected'
-      clinical_trial.save
+      clinical_trial.save(:validate => false)
     end
 
     # What's left?
-    remaining_trials = trials_for_randomization
+    remaining_trials = clinical_trials - trials_for_randomization
 
     # Sample 46 trials from the remaining trials for the control group
-    control_group_trials = remaining_trials.sample(46, random)
+    control_group_trials = remaining_trials.sample(46, random: random)
     control_group_trials.each do |clinical_trial|
       clinical_trial.randomization_status = 'Control'
-      clinical_trial.save
+      clinical_trial.save(:validate => false)
     end
 
     # What's left?
-    unused_trials = remaining_trials = control_group_trials
+    unused_trials = remaining_trials - control_group_trials
     unused_trials.each do |clinical_trial|
       clinical_trial.randomization_status = 'Unused'
-      clinical_trial.save
+      clinical_trial.save(:validate => false)
     end
   end
 end
