e2110054df81836b95c7e6723e4da88e93f50ea4
Praveen Angyan <angyan@usc.edu>
2015-07-23 15:52:07 -0700

    Adding in code to generate organic message timeline

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,4 +3,65 @@ class Message < ActiveRecord::Base
 
   belongs_to :message_template
   belongs_to :clinical_trial
+
+  def self.generate(start_date)
+    # TODO: Unit test
+    Message.destroy_all
+    published_at = start_date
+    clinical_trials = ClinicalTrial.where(:randomization_status => 'Selected')
+    twitter_awareness_message_templates = MessageTemplate.where(:message_type => 'awareness', :platform => 'twitter').to_a
+    twitter_recruiting_message_templates = MessageTemplate.where(:message_type => 'recruiting', :platform => 'twitter').to_a
+    facebook_awareness_message_templates = MessageTemplate.where(:message_type => 'awareness', :platform => 'facebook').to_a
+    facebook_recruiting_message_templates = MessageTemplate.where(:message_type => 'recruiting', :platform => 'facebook').to_a
+    random = Random.new
+    Bitly.use_api_version_3
+    Bitly.configure do |config|
+      config.api_version = 3
+      config.access_token = '21c4a40d1746ea7d0815aa33a9a3137c50c389e8'
+    end
+    url_shortener = UrlShortener.new
+
+    WebMock.allow_net_connect!
+
+    clinical_trials.each do |clinical_trial|
+      # Organic message + image + awareness message template
+      # Organic message + image + recruitment message template
+
+      # Twitter: Organic message + no image + awareness message template
+      message = Message.new
+      message.clinical_trial = clinical_trial
+      message.message_template = twitter_awareness_message_templates.sample(1, random: random)[0]
+      message.content = message.message_template.content.gsub('<%= message[:url] %>', url_shortener.shorten(clinical_trial.url))
+      message.published_at = start_date
+      message.save
+
+      # Twitter: Organic message + no image + recruitment message template
+      message = Message.new
+      message.clinical_trial = clinical_trial
+      message.message_template = twitter_recruiting_message_templates.sample(1, random: random)[0]
+      message.content = message.message_template.content.gsub('<%= message[:url] %>', url_shortener.shorten(clinical_trial.url))
+      message.published_at = start_date
+      message.save
+
+      # Facebook: Organic message + no image + awareness message template
+      message = Message.new
+      message.clinical_trial = clinical_trial
+      message.message_template = facebook_awareness_message_templates.sample(1, random: random)[0]
+      message.content = message.message_template.content.gsub('<%= message[:url] %>', url_shortener.shorten(clinical_trial.url))
+      message.published_at = start_date
+      message.save
+
+      # Facebook: Organic message + no image + recruitment message template
+      message = Message.new
+      message.clinical_trial = clinical_trial
+      message.message_template = facebook_recruiting_message_templates.sample(1, random: random)[0]
+      message.content = message.message_template.content.gsub('<%= message[:url] %>', url_shortener.shorten(clinical_trial.url))
+      message.published_at = start_date
+      message.save
+
+      start_date = start_date + 1
+    end
+
+    WebMock.disable_net_connect!
+  end
 end
--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 describe Message do
   it { is_expected.to validate_presence_of(:content) }
 
-  # Source - platform
-  # Medium - ad, feed
+  # Source - Platform
+  # Medium - Ad, Organic
   # Campaign - Trial Promoter Staging or Trial Promoter
 
   it { is_expected.to belong_to :clinical_trial }
