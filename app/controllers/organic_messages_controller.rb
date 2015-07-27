1880e9079b39d62dc6b8e467c6e2e758df17f5f0
Praveen Angyan <angyan@usc.edu>
2015-07-27 16:02:24 -0700

    Fixing ordering of display of organic and paid messages

--- app/controllers/organic_messages_controller.rb
+++ app/controllers/organic_messages_controller.rb
@@ -1,6 +1,6 @@
 class OrganicMessagesController < ApplicationController
   def index
-    @twitter_organic_messages = Message.joins("inner join message_templates on messages.message_template_id = message_templates.id").where("message_templates.platform = ? and messages.medium = ?", 'twitter', 'organic')
-    @facebook_organic_messages = Message.joins("inner join message_templates on messages.message_template_id = message_templates.id").where("message_templates.platform = ? and messages.medium = ?", 'facebook', 'organic')
+    @twitter_organic_messages = Message.joins("inner join message_templates on messages.message_template_id = message_templates.id").where("message_templates.platform = ? and messages.medium = ?", 'twitter', 'organic').order('scheduled_at')
+    @facebook_organic_messages = Message.joins("inner join message_templates on messages.message_template_id = message_templates.id").where("message_templates.platform = ? and messages.medium = ?", 'facebook', 'organic').order('scheduled_at')
   end
 end
--- app/controllers/paid_messages_controller.rb
+++ app/controllers/paid_messages_controller.rb
@@ -1,6 +1,6 @@
 class PaidMessagesController < ApplicationController
   def index
-    @twitter_paid_messages = Message.joins("inner join message_templates on messages.message_template_id = message_templates.id").where("message_templates.platform = ? and messages.medium = ?", 'twitter', 'paid')
-    @facebook_paid_messages = Message.joins("inner join message_templates on messages.message_template_id = message_templates.id").where("message_templates.platform = ? and messages.medium = ?", 'facebook', 'paid')
+    @twitter_paid_messages = Message.joins("inner join message_templates on messages.message_template_id = message_templates.id").where("message_templates.platform = ? and messages.medium = ?", 'twitter', 'paid').order('scheduled_at')
+    @facebook_paid_messages = Message.joins("inner join message_templates on messages.message_template_id = message_templates.id").where("message_templates.platform = ? and messages.medium = ?", 'facebook', 'paid').order('scheduled_at')
   end
 end
