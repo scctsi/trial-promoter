a8707162dd65e98bb7a46e9179451df1b99c96a1
Praveen Angyan <angyan@usc.edu>
2015-10-09 13:45:10 -0700

    Running migration for RAILS_ENV=test

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151009190741) do
+ActiveRecord::Schema.define(version: 20151009201550) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -79,6 +79,7 @@ ActiveRecord::Schema.define(version: 20151009190741) do
     t.string   "buffer_update_id"
     t.text     "statistics"
     t.string   "service_update_id"
+    t.text     "service_statistics"
   end
 
   create_table "platforms", force: :cascade do |t|
