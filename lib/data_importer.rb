c7a31451e1904e3c35992f9e99f0a0314b8b2a1a
Praveen Angyan <angyan@usc.edu>
2015-10-09 15:27:44 -0700

    Fixing bug where a missing tweet would cause a crash of importing analytics from Twitter

--- lib/data_importer.rb
+++ lib/data_importer.rb
@@ -84,9 +84,11 @@ class DataImporter
     csv_text = File.read(Rails.root.join('data_dumps', 'twitter_activity_metrics_20150901_20151001.csv'))
     csv = CSV.parse(csv_text, :headers => true)
     csv.each do |row|
-      message = Message.where('service_update_id = ?', row[0].to_s)[0]
-      message.service_statistics = { 'retweets' => row[7], 'favorites' => row[9], 'replies' => row[8], 'clicks' => row[11], 'user_profile_clicks' => row[10], 'impressions' => row[4]}
-      message.save
+      twitter_messages = Message.where('service_update_id = ?', row[0].to_s)
+      if twitter_messages != nil
+        twitter_messages[0].service_statistics = { 'retweets' => row[7], 'favorites' => row[9], 'replies' => row[8], 'clicks' => row[11], 'user_profile_clicks' => row[10], 'impressions' => row[4]}
+        twitter_messages[0].save
+      end
     end
   end
 
